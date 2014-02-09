ae822ceab6c8f8fed24ea3b7da69e773b7b35873
whirlwin <whirlwin90@gmail.com>
2014-02-09 17:09:32 +0100

    Add Optional example.

---
+++ src/main/java/org/whirlwin/java8/examples/misc/OptionalExample.java
@@ -0,0 +1,33 @@
+package org.whirlwin.java8.examples.misc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class OptionalExample {
+
+    private static final Map<Integer, String> nameRegistry = new HashMap<Integer, String>() {{
+        put(1, "John Doe");
+    }};
+
+    public static void main(String[] args) {
+        Optional<String> johnDoe = getNameById(1);
+        printName(johnDoe);
+
+        Optional<String> janeDoe = getNameById(2);
+        printName(janeDoe);
+    }
+
+    public static Optional<String> getNameById(int id) {
+        String name = nameRegistry.get(id);
+        return Optional.ofNullable(name);
+    }
+
+    public static void printName(Optional<String> name) {
+        if (name.isPresent()) {
+            System.out.println(name.get());
+        } else {
+            System.out.println("Warning: Name not set");
+        }
+    }
+}
